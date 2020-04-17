c23137da1e3101ba3aa8ff33594b5c754d86dcce
AlexMelian <57376895+AlexMelian@users.noreply.github.com>
2020-04-17 05:29:42 -0300

    V11

---
+++ javascript/index.js
@@ -0,0 +1,32 @@
+x[1].style.opacity = "0.5"
+x[0].style.opacity = "1"
+
+function currentDiv(n) {
+  var x = document.getElementsByClassName("demo");
+  if (n == 1) {
+    x[1].style.opacity = "0.5"
+    x[0].style.opacity = "1"
+
+  } else {
+    x[0].style.opacity = "0.5"
+    x[1].style.opacity = "1"
+  }
+
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
+  }
+  x[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " w3-opacity-off";
+}
